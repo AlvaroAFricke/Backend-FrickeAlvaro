aa1b271d85cdee46d315407d8f0ee281f406e5de
AlviAFricke <alvaroafricke@gmail.com>
2022-12-07 16:59:53 -0300

    Proyecto Final

---
+++ ProyectoFinal/TEST.js
@@ -0,0 +1,17 @@
+let persona = {
+    nom:"Alvaro"
+}
+
+const personas = new Array();
+
+personas.push(persona)
+
+persona.nom = "Fran"
+
+personas.push(persona)
+
+console.log(personas)
+
+persona.nom = "mateo"
+
+console.log(personas)
